0e6383e1b00faa1753a32f95e44f0fc4539f52e1
Harsh Kushwah <49446555+hrsh-4@users.noreply.github.com>
2021-01-02 23:50:05 +0530

    Update urls.py

--- nursery/urls.py
+++ nursery/urls.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
-from django.urls import path,include
+
 
 from nursery import views
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
-
+from django.contrib import admin 
+from django.urls import path 
+from django.conf import settings 
+from django.conf.urls.static import static
 urlpatterns = [
 
 	path("", views.plant_list ,name="home"),
@@ -30,7 +30,7 @@ urlpatterns = [
 
 
 ]
-
+ 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
